d34dbbfb7ca6ba21fdb091bbfd9a762f95d6f999
Kenneth Leung <kennethleung.ty@gmail.com>
2021-12-09 22:03:12 +0800

    Setup parsing between FastAPI and Streamlit UI

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from fastapi import FastAPI, File, UploadFile
-from fastapi.encoders import jsonable_encoder
-from fastapi.responses import JSONResponse
+# ===========================
+# Module: FastAPI setup
+# Author: Kenneth Leung
+# Last Modified: 6 Dec 2021
+# ===========================
+# Command to execute script: uvicorn main:app --host=0.0.0.0 --port=8000
 
 import pandas as pd
+import io
 import h2o
+
+from fastapi import FastAPI, File
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse, HTMLResponse
 
 import mlflow
 import mlflow.h2o
@@ -29,31 +37,46 @@ best_model = mlflow.h2o.load_model(f"mlruns/{exp_id}/{run_id}/artifacts/model/")
 
 # Create POST endpoint with path '/predict'
 @app.post("/predict")
-async def predict(file: UploadFile = File(...)):
-    if file.filename.endswith(".csv"):
-        with open(file.filename, "wb") as f:
-            f.write(file.file.read())
-        test_df = pd.read_csv(file.filename)
-        test_h2o = h2o.H2OFrame(test_df)
+# async def predict(file: UploadFile = File(...)):
+async def predict(file: bytes = File(...)):
+    print('[+] Initiate Prediction')
+    file_obj = io.BytesIO(file)
+    test_df = pd.read_csv(file_obj)
+    test_h2o = h2o.H2OFrame(test_df)
 
-        # Separate ID column (if any)
-        id_name, X_id, X_h2o = separate_id_col(test_h2o)
+    # Separate ID column (if any)
+    id_name, X_id, X_h2o = separate_id_col(test_h2o)
 
-        # Match test set col types with train set
-        X_h2o = match_col_types(X_h2o)
+    # Match test set col types with train set
+    X_h2o = match_col_types(X_h2o)
 
-        # Generate predictions with best model (output is H2O frame)
-        preds = best_model.predict(X_h2o)
-        
-        if id_name is not None:
-            preds_list = preds.as_data_frame()['predict'].tolist()
-            id_list = X_id.as_data_frame()[id_name].tolist()
-            preds_final = dict(zip(id_list, preds_list))
-        else:
-            preds_final = preds.as_data_frame()['predict'].tolist()
-
-        json_compatible_item_data = jsonable_encoder(preds_final)
-        return JSONResponse(content=json_compatible_item_data)
-        
+    # Generate predictions with best model (output is H2O frame)
+    preds = best_model.predict(X_h2o)
+    
+    # Apply processing if dataset has ID column
+    if id_name is not None:
+        preds_list = preds.as_data_frame()['predict'].tolist()
+        id_list = X_id.as_data_frame()[id_name].tolist()
+        preds_final = dict(zip(id_list, preds_list))
     else:
-        raise ValueError(f'Uploaded file is not a valid CSV file')
+        preds_final = preds.as_data_frame()['predict'].tolist()
+
+    json_compatible_item_data = jsonable_encoder(preds_final)
+    return JSONResponse(content=json_compatible_item_data)
+
+@app.get("/")
+async def main():
+    content = """
+    <body>
+    <h2> Welcome to the End to End AutoML Project
+    </body>
+    """
+    # content = """
+    # <body>
+    # <form action="/predict/" enctype="multipart/form-data" method="post">
+    # <input name="file" type="file" multiple>
+    # <input type="submit">
+    # </form>
+    # </body>
+    # """
+    return HTMLResponse(content=content)
---
+++ ui.py
@@ -0,0 +1,45 @@
+# =========================================
+# H2O AutoML Training with MLflow Tracking
+# Author: Kenneth Leung
+# =========================================
+# Command to execute script: streamlit run ui.py
+
+import streamlit as st
+import requests
+import pandas as pd
+import io
+import json
+
+st.title('End-to-End AutoML Project: Life Insurance Assessment')
+
+# FastAPI endpoint
+endpoint = 'http://localhost:8000/predict'
+st.text('''Author: Kenneth Leung''') # description and instructions
+
+test_csv = st.file_uploader('', type=['csv','xlsx'], accept_multiple_files=False)
+
+if test_csv:
+    test_df = pd.read_csv(test_csv)
+    st.subheader('View Sample of Test Set')
+    st.write(test_df.head())
+
+    test_bytes_obj = io.BytesIO()
+    test_df.to_csv(test_bytes_obj, index=False)  # write to BytesIO buffer
+    test_bytes_obj.seek(0) # Reset pointer to avoid EmptyDataError
+
+    files = {"file": ('test_dataset.csv', test_bytes_obj, "multipart/form-data")}
+
+    if st.button('Start Prediction'):
+        if len(test_df) == 0:
+            st.write("Please upload a valid test dataset!")  # handle case with no image
+        else:
+            with st.spinner('Prediction in Progress. Please Wait...'):
+                output = requests.post(endpoint, 
+                                       files=files,
+                                       timeout=8000)
+            st.success('Success! Click the Download button below to retrieve prediction results (JSON format')
+            st.download_button(
+                label='Download',
+                data=json.dumps(output.json()), # Download as JSON file object
+                file_name='automl_prediction_results.json'
+            )
